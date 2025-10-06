e3fb87f826ef50692bb4f733e5c5364b3022ad64
Joel <Joelsari19@gmail.com>
2025-10-06 14:52:56 -0400

    removed unnecessary app folder

--- moviemaps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'moviemaps'
-urlpatterns = [
-    path('geocoding/<int:pk>/', GeocodingView.as_view(), name='geocode'),
-    path('', lambda request: render(request, 'moviemaps/geocoding_home.html'), name='map_home'),
-]
